c4795a521ab2a62634350fd6dae39aae5ef19d27
CabbaGG2 <jcamaradepablos@danielcastelao.org>
2025-11-05 13:00:02 +0100

    Creada la clase de Datos con los estados de juego close #8

---
+++ app/src/main/java/jc/dam/damiandice/Datos.kt
@@ -0,0 +1,33 @@
+package jc.dam.damiandice
+
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.flow.MutableStateFlow
+
+/**
+ * Clase para almacenar los datos del juego
+ */
+object Datos {
+    var numero = 0
+    val victorias = MutableStateFlow(0)
+    var derrotas = MutableStateFlow(0)
+}
+
+/**
+ * Colores utilizados
+ */
+enum class Colores(val color: Color, val txt: String) {
+    CLASE_ROJO(color = Color.Red, txt = "roxo"),
+    CLASE_VERDE(color = Color.Green, txt = "verde"),
+    CLASE_AZUL(color = Color.Blue, txt = "azul"),
+    CLASE_AMARILLO(color = Color.Yellow, txt = "melo"),
+    CLASE_START(color = Color.Magenta, txt = "Start")
+}
+
+/**
+ * Estados del juego
+ */
+enum class Estados(val start_activo: Boolean, val boton_activo: Boolean) {
+    INICIO(start_activo = true, boton_activo = false),
+    GENERANDO(start_activo = false, boton_activo = false),
+    ADIVINANDO(start_activo = false, boton_activo = true),
+}
